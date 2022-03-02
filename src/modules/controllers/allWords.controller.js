1f3be91f7309789c5528261ea15dc7a34176265b
Andpogosyan <apogosyan.exceedteam@gmail.com>
2022-03-02 15:49:18 +0300

    create login and register

--- src/modules/controllers/allWords.controller.js
+++ src/modules/controllers/allWords.controller.js
@@ -1,9 +1,12 @@
+const allWordsSchema = require('../../db/models/allWords-schema/index');
 const AllWordsSchema = require('../../db/models/allWords-schema/index');
 const ShownWordsSchema = require('../../db/models/shownWords-schema')
 const {
     allTranslationsArray,
     allWordsArray
 } = require('../../services/allWords.services');
+const jwt = require('jsonwebtoken')
+
 
 module.exports.postAllWordsToDb = async (req, res) => {
     try {
@@ -31,10 +34,17 @@ module.exports.getRandomWord = async (req, res) => {
     try {
         const result = await AllWordsSchema.find();
         const index = Math.floor(Math.random() * result.length);
+        const token  = req.headers.authorization;
+        
+        let payload;
 
-        res.status(200).send({
-            word: result[index].word,
-            translation: result[index].translation,
+        payload = jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+            !err ? res.status(200).send({
+                     word: result[index].word,
+                     translation: result[index].translation,
+                     tokenRes: decoded
+                 }) :
+                 res.status(500).send(err)
         });
     } catch (e) {
         res.status(500).send({ message: '22' });
@@ -78,6 +88,30 @@ module.exports.handleStudiedWord = async (req, res) => {
             .then(() => {
                 res.status(400).send('Success');
             })
+    } catch (e) {
+        res.status(500).send({ message: 'internal server error' });
+    }
+}
+
+
+module.exports.addNewWord = async (req, res) => {
+    try {
+        const { word, translation } = req.body;
+
+        const checkWord = await allWordsSchema.find({word: word})
+
+        if(checkWord.length > 0){
+            res.status(403).json({message: 'This word already exists'})
+        } else {
+            const newWord = new AllWordsSchema({
+                word: word,
+                translation: translation
+            })
+
+            await newWord.save().then(result => {
+                res.status(200).json(result)
+            })
+        }
     } catch (e) {
         res.status(500).send({ message: 'internal server error' });
     }
--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -4,11 +4,27 @@ const router = express.Router();
 const {
     handleStudiedWord,
     postAllWordsToDb,
-    getRandomWord
+    getRandomWord,
+    addNewWord
 } = require('../controllers/allWords.controller');
+
+const {
+    createNewUser,
+    login
+} = require('../controllers/user.controller')
+
+// words routes
 
 router.get('/getRandomWord', getRandomWord);
 router.post('/postAllWordsToDb', postAllWordsToDb);
 router.patch('/handleStudiedWord', handleStudiedWord);
+router.post('/addNewWord', addNewWord)
+
+// users routes
+
+router.post('/register', createNewUser);
+
+router.post('/login', login)
+
 
 module.exports = router;
